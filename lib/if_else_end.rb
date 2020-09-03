c8c77badcc1c1d894738c0f4d417fcb1397c616c
Mohammad S Hussain <m.s.hussain@outlook.com>
2020-09-03 22:07:42 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,9 +1,11 @@
+require 'pry'
 if <something that is either true or false>
   if true
   puts "This code runs!"
 end
 run_code_inside = false
 puts "Code before if...end"
+binding_pry
 if run_code_inside
   puts "code inside"
 end
