6c085af645aa232cab9a983608d34920eb1db3d0
Mohammad S Hussain <m.s.hussain@outlook.com>
2020-09-03 21:47:31 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,10 @@
-# Write your solution here
+if <something that is either true or false>
+  if true
+  puts "This code runs!"
+end
+run_code_inside = false
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
+end
+puts "Code after if...end"
